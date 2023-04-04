5c81f700ce62b3290119afc77d4db23cdddcd176
Dildora <48212@edu.raseko.fi>
2023-04-05 02:07:22 +0300

    lisä
    lisä

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 import pygame
 
+
+DEFAULT_SCREEN_SIZE = (800, 450)
+FPS_TEXT_COLOR = (128, 0, 128) #dark  blue
+
 def main():
     game = Game()
     game.run()
@@ -8,38 +12,57 @@ def main():
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen_w = self.screen.get_width()
+        self.screen_h = self.screen.get_height()
+        self.screen = pygame.display.set_mode((DEFAULT_SCREEN_SIZE))
         self.running = False
+        self.font16 = pygame.font.Font("fonts/SyneMoto-Regular.ttf", 16)
         self.init_graphics()
         self.init_objects()
         
-    def init_graphics(self):
-        self.bird_frame = 0
-        bird_imgs = [
+    def init_graphics(self):        
+        original_bird_imges = [
             pygame.image.load(f"images/chicken/flying/frame-{i}.png")
             for i in [1, 2 ,3, 4]
         ]
         self.bird_imgs = [
-            pygame.transform.rotozoom(x, 0, 1/16)
-            for x in bird_imgs
-        ]  
-        bg_imgs = [
+            pygame.transform.rotozoom(x, 0, self.screen_h / 9600).convert_alpha()
+            for x in original_bird_imges
+        ] 
+        
+        original_bird_dead_images  = [
+            pygame.image.load(f"images/chicken/got_hit/frame-{i}.png").convert_alpha()
+            for i in [1, 2]
+        ]
+        
+        self.bird_dead_imgs = [
+            pygame.transform.rotozoom(x, 0, self.screen_h / 9600)
+            for x in original_bird_dead_images]
+        
+        original_bg_imges = [
             pygame.image.load(f"images/background/layer_{i}.png")
             for i in [1, 2 ,3]
             ]
         self.bg_imgs = [
-            pygame.transform.rotozoom(x, 0, 600 / x.get_height()).convert_alpha()
-            for x in bg_imgs
+            pygame.transform.rotozoom(img, 0, self.screen_h / img.get_height()).convert_alpha()
+            for img in original_bg_imges
         ]  
+        self.bg_widths = [img.get_width() for img in self.bg_imgs]
      
     def init_objects(self):
         self.bird_y_speed = 0
-        self.bird_pos = (200, 000)
+        self.bird_alive = True
+        self.bird_pos = (self.screen_w / 3, self.screen_h / 2)
+        self.bird_angle = 0
+        self.bird_frame = 0
         self.bird_lift = False
-        self.bg0_pos = 0
-        self.bg1_pos = 0
-        self.bg2_pos = 0
-
+        self.bg_pos = [0, 0, 0]  
+        
+    def scale_positions(self, scale_x, scale_y):
+        self.bird_pos = (self.bird_pos[0] * scale_x, self.bird_pos[1] * scale_y)
+        self.bird_pos[0] = self.bird_pos[0] * scale_x
+        self.bird_pos[1] = self.bird_pos[1] * scale_x
+        self.bird_pos[2] = self.bird_pos[2] * scale_x
        
     def run(self):                 
         clock = pygame.time.Clock()
@@ -66,43 +89,98 @@ class Game:
                 elif event.type == pygame.KEYUP:
                     if event.key in (pygame.K_SPACE, pygame.K_UP):
                         self.bird_lift = False
+                    elif event.key in (pygame.K_f, pygame.K_F11):
+                        self.toggle_fullscreen()
+                        
+    def toggle_fullscreen(self):
+        self.is_fullscreen = not self.is_fullscreen
+        old_w = self.screen_w
+        old_h = self.screen_h
+        if self.is_fullscreen:
+            pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        else:
+            pygame.display.set_mode(DEFAULT_SCREEN_SIZE)
+            screen = pygame.display.get_surface()
+            self.screen_w = screen.get_width()
+            self.screen_h = screen.get_height()
+            self.init_graphics(
+                scale_x = (self.screen_w / old_w),
+                scale_y = (self.screen_h / old_h),
+            )
+            
         
     def handle_game_logic(self):
-        self.bg0_pos -= 0.25
-        self.bg1_pos -= 0.5
-        self.bg2_pos -= 2
+        if self.bird_alive:
+            self.bg_pos[0] -= 0.5
+            self.bg_pos[1] -= 1
+            self.bg_pos[2] -= 3
 
         bird_y = self.bird_pos[1]        
         
-        if self.bird_lift:
+        if self.bird_lift and self.bird_alive:
             # Lintua nostetaan (0.5 px nostovauhtia / frame)
             self.bird_y_speed -= 0.5
-            self.bird_frame += 1
         else:
             #Painovoima (lisää putoamisnopeutta joka kuvassa)
             self.bird_y_speed += 0.2 
+            
+        if self.bird_lift or not self.bird_alive:
+            self.bird_frame += 1
 
         #Liikuta lintua sen nopeuden verran    
         bird_y += self.bird_y_speed 
-
+        
+        if self.bird_alive:  
+            #Laske linnun asento
+            self.bird_angle = -90 * 0.04 * self.bird_y_speed
+            self.bird_angle  = max(min(self.bird_angle, 60), -60)  
+        
+        # Tarkista  onko lintu pudonnut maahan. Skalattu (600-135)/600 = 0.78
+        if bird_y > self.screen_h * 0.78:
+            bird_y = self.screen_h * 0.78
+            self.bird_y_speed = 0
+            self.bird_alive = False
+            
+        if bird_y < 10:
+            self.bird_alive = False
+        
+            #Laske linnun asento
+            self.bird_angle = -90 * 0.04 * self.bird_y_speed
+            self.bird_angle  = max(min(self.bird_angle, 60), -60) 
+                
+        # Asenna linnun x-y- koordinatit self.bird_pos-muuttujaan
         self.bird_pos = (self.bird_pos[0], bird_y)
                 
     def update_screen(self):
         # Taytä tausta vaaleansinisellä  
-        self.screen.fill((230, 230, 255))
-
-        self.screen.blit(self.bg_imgs[0], (self.bg0_pos, 0))
-        self.screen.blit(self.bg_imgs[1], (self.bg1_pos, 0))
-        self.screen.blit(self.bg_imgs[2], (self.bg2_pos, 0))
+        #self.screen.fill((230, 230, 255))
+        
+        # Piirrä taustakerrokset (3 kpl)
+        for i in [0, 1, 2]: 
+            # Ensin piirrä vasen tausta 
+            self.screen.blit(self.bg_imgs[i], (self.bg_pos[i], 0))
+            # Jos tausta ei riitä peittämään koko ruutua, niin ...
+        if self.bg_pos[i] + self.bg_widths[i] < self.screen_w:
+            #... piirrä sama tausta vielä oikealle puolelle
+            self.screen.blit(self.bg_imgs[i], (self.bg_pos[i] + self.bg_widths[i], 0))
+            # Jos taustaa on jo siirretty sen leveyden verran....
+        if self.bg_pos[0] < -self.bg_widths[i]:
+            # ... niin aloita alusta
+            self.bg_pos[0] += self.bg_widths[i]
         
         #Pirrä lintu
-        angle = -90 * 0.04 * self.bird_y_speed
-        angle = max(min(angle, 60), -60) 
-
-        bird_img_i = self.bird_imgs[(self.bird_frame // 3) % 4]
-        
-        bird_img= pygame.transform.rotozoom(bird_img_i, angle, 1)
+        if self.bird_alive:
+            bird_img_i = self.bird_imgs[(self.bird_frame // 3) % 4]
+        else:
+            bird_img_i = self.bird_dead_imgs[(self.bird_frame // 3) % 2]
+        bird_img= pygame.transform.rotozoom(bird_img_i, self.bird_angle, 1)
         self.screen.blit(bird_img, self.bird_pos)
+        
+        # Piirrä fps luku
+        if self.show_fps:
+            fps_txt = f"{self.clock.get_fps():.1f}fps"
+            fps_img = self.font16.render(fps_txt, True, FPS_TEXT_COLOR)
+            self.screen.blit(fps_img, (0, 0))
         
         pygame.display.flip()
 
